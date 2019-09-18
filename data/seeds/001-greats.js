71f0a2b207bb12c5646f1a2e1e2c0b56c656450e
andrew dillon <majik895@gmail.com>
2019-09-18 09:56:43 -0400

    :football: Migrations & seeds complete

--- data/seeds/001-greats.js
+++ data/seeds/001-greats.js
@@ -1,4 +1,3 @@
-
 exports.seed = function(knex, Promise) {
 	// Deletes ALL existing entries and resets ids
 	return knex('greats')
@@ -8,19 +7,19 @@ exports.seed = function(knex, Promise) {
 				{ name: 'Walter Payton' },
 				{ name: 'Joe Montana' },
 				{ name: 'Mike Singletary' },
-                { name: 'Jack Lambert' }
-                { name: 'Dan Marino' }
-                { name: 'Roger Staubach' }
-                { name: 'Eric Dickerson' }
-                { name: 'Ray Lewis' }
-                { name: 'Reggie White' }
-                { name: 'Barry Sanders' }
-                { name: 'Lawrence Taylor' }
-                { name: 'Peyton Manning' }
-                { name: 'Mean Joe Green' }
-                { name: 'Jim Brown' }
-                { name: 'Tom Brady' }
-                { name: 'Jerry Rice' }
+				{ name: 'Jack Lambert' },
+				{ name: 'Dan Marino' },
+				{ name: 'Roger Staubach' },
+				{ name: 'Eric Dickerson' },
+				{ name: 'Ray Lewis' },
+				{ name: 'Reggie White' },
+				{ name: 'Barry Sanders' },
+				{ name: 'Lawrence Taylor' },
+				{ name: 'Peyton Manning' },
+				{ name: 'Mean Joe Green' },
+				{ name: 'Jim Brown' },
+				{ name: 'Tom Brady' },
+				{ name: 'Jerry Rice' }
 			])
 		})
 }
