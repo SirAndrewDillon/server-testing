243841db9264232e47f2164a45e6b29d9e4744e5
andrew dillon <majik895@gmail.com>
2019-09-18 08:25:37 -0400

    :alien: Setup testing on .env

---
+++ api/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const Hobbits = require('../hobbits/hobbitsModel.js')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+	res.status(200).json({ api: 'up' })
+})
+
+server.get('/hobbits', (req, res) => {
+	Hobbits.getAll()
+		.then((hobbits) => {
+			res.status(200).json(hobbits)
+		})
+		.catch((error) => {
+			const err = {
+				message: error.message,
+				stack: error.stack
+			}
+			res.status(500).json(err)
+		})
+})
+
+module.exports = server
